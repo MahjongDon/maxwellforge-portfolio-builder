c993ab8cc14e355d6d1da733545a91f83e28cf56
MahjongDon <samuelbeacon222@gmail.com>
2025-03-31 22:44:14 +1030

    deleted:    dist/fin-forge/_redirects 	deleted:    dist/forge-coffee/_redirects 	deleted:    dist/forge-crm/_redirects 	deleted:    dist/forge-notes/_redirects 	deleted:    dist/forge-pass-safeguard/_redirects 	deleted:    dist/forge-sales/_redirects 	deleted:    dist/forge-wander/_redirects 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const projects = [
   'fin-forge',
